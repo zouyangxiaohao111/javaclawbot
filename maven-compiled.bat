cdb5c68bc5c73efc5450f079b9135cff4ddc0a0e
zcw <1924750151@qq.com>
2026-06-08 18:24:16 +0800

    feat（*）： 新增开源工具技能包

--- maven-compiled.bat
+++ maven-compiled.bat
@@ -7,8 +7,8 @@ echo [%date% %time%] Maven compile started...
 echo ================================================================
 echo [%date% %time%] Maven compile started... > "%LOGFILE%"
 
-:: 直接输出到控制台，Bash 工具可直接读取，无需再查日志文件
-"C:\Program Files\Java\jdk-17\bin\java.exe" -Dmaven.multiModuleProjectDirectory=D:\code\ai_project\javaclawbot -Djansi.passthrough=true -Dmaven.home=D:\IDEA20240307\plugins\maven\lib\maven3 -Dclassworlds.conf=D:\IDEA20240307\plugins\maven\lib\maven3\bin\m2.conf -Dmaven.ext.class.path=D:\IDEA20240307\plugins\maven\lib\maven-event-listener.jar -javaagent:D:\IDEA20240307\lib\idea_rt.jar=53924 -Dfile.encoding=UTF-8 -classpath D:\IDEA20240307\plugins\maven\lib\maven3\boot\plexus-classworlds-2.8.0.jar;D:\IDEA20240307\plugins\maven\lib\maven3\boot\plexus-classworlds.license org.codehaus.classworlds.Launcher -Didea.version=2024.3.7 -Dmaven.repo.local=D:\apps\maven\repository package
+:: Updated 2026-06-08: switched from IDEA 2024.03.07 bundled Maven to standalone Maven 3.8.1
+"C:\Program Files\Java\jdk-17\bin\java.exe" -Dmaven.multiModuleProjectDirectory=D:\code\ai_project\javaclawbot -Djansi.passthrough=true -Dmaven.home=D:\apps\maven\maven-3.8.1 -Dclassworlds.conf=D:\apps\maven\maven-3.8.1\bin\m2.conf -Dfile.encoding=UTF-8 -classpath D:\apps\maven\maven-3.8.1\boot\plexus-classworlds-2.6.0.jar;D:\apps\maven\maven-3.8.1\boot\plexus-classworlds.license org.codehaus.classworlds.Launcher -Dmaven.repo.local=D:\apps\maven\repository package
 
 set EXITCODE=%ERRORLEVEL%
 
